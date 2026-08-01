ea11322ce343da2a4ffabd42123b735c511df450
ChotuX143 <a2zmobilesolutions96@gmail.com>
2026-08-01 20:27:52 +0530

    ɪᴛ's ᴍʀ ᴘᴇʀғᴇᴄᴛ [ᴀꜰᴋ] ✘™ ☘️ 🕊⚡️

--- PritiMusic/core/userbot.py
+++ PritiMusic/core/userbot.py
@@ -46,9 +46,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("https://t.me/betabot_hub")
-                await self.one.join_chat("https://t.me/betabot_support")
-                await self.one.join_chat("https://t.me/sukoon_s")
+                await self.one.join_chat("https://t.me/TitanNetwrk")
+                await self.one.join_chat("https://t.me/Musical_Beatsz")
+                await self.one.join_chat("https://t.me/Telugu_Grpz")
             except:
                 pass
             assistants.append(1)
@@ -69,9 +69,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("https://t.me/betabot_hub")
-                await self.two.join_chat("https://t.me/betabot_support")
-                await self.two.join_chat("https://t.me/sukoon_s")
+                await self.two.join_chat("https://t.me/TitanNetwrk")
+                await self.two.join_chat("https://t.me/Musical_Beatsz")
+                await self.two.join_chat("https://t.me/Telugu_Grpz")
             except:
                 pass
             assistants.append(2)
@@ -92,9 +92,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("https://t.me/betabot_hub")
-                await self.three.join_chat("https://t.me/betabot_support")
-                await self.three.join_chat("https://t.me/sukoon_s")
+                await self.three.join_chat("https://t.me/TitanNetwrk")
+                await self.three.join_chat("https://t.me/Musical_Beatsz")
+                await self.three.join_chat("https://t.me/Telugu_Grpz")
             except:
                 pass
             assistants.append(3)
@@ -115,9 +115,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("https://t.me/betabot_hub")
-                await self.four.join_chat("https://t.me/betabot_support")
-                await self.four.join_chat("https://t.me/sukoon_s")
+                await self.four.join_chat("https://t.me/TitanNetwrk")
+                await self.four.join_chat("https://t.me/Musical_Beatsz")
+                await self.four.join_chat("https://t.me/Telugu_Grpz")
             except:
                 pass
             assistants.append(4)
